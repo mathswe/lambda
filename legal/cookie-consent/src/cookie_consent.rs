893539270e55bbe2b4a3be93b3573250103c275f
Tobias Briones <tobiasbriones.dev@gmail.com>
2024-03-09 19:24:13 -0600

    Implement initial CloudFare Worker microservice

--- legal/cookie-consent/src/cookie_consent.rs
+++ legal/cookie-consent/src/cookie_consent.rs
@@ -1,2 +1,25 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/lambda
+
+use worker::{console_log, Error, Request, Response, RouteContext};
+
+use crate::consent::CookieConsentPref;
+
+pub async fn post_consent_pref(
+    mut req: Request,
+    _ctx: RouteContext<()>,
+) -> Result<Response, Error> {
+    req
+        .json::<CookieConsentPref>()
+        .await
+        .map(|pref| register_consent(req, pref))
+        .map_err(|e|
+            Error::from(format!("Fail to read request JSON body: {}", e))
+        )
+}
+
+fn register_consent(req: Request, pref: CookieConsentPref) -> Response {
+    console_log!("Received request: {:?}", req);
+
+    Response::from_json(&pref).unwrap()
+}
--- legal/cookie-consent/src/lib.rs
+++ legal/cookie-consent/src/lib.rs
@@ -1,5 +1,19 @@
 // Copyright (c) 2024 Tobias Briones. All rights reserved.
 // This file is part of https://github.com/mathswe/lambda
 
+use worker::*;
+
+use crate::cookie_consent::post_consent_pref;
+
 mod consent;
 mod cookie_consent;
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    let router = Router::new();
+
+    router
+        .post_async("/", post_consent_pref)
+        .run(req, env)
+        .await
+}
